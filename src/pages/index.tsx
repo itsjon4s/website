8b989497aadc530c259df8f1989069c01105931a
itsjon4s <motajoao670@gmail.com>
2022-08-01 15:54:21 +0100

    fix: fonts too small

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,23 @@
 import Head from "next/head";
-import { Github, Discord, Spotify } from "../components/socials";
+import { Github, Discord, Spotify } from "../components/Socials";
 
 export default function Home() {
   return (
     <main className="h-screen w-screen bg-[#17191a]">
       <Head>
-        <title>Jonas</title>
+        <title>Jon4s</title>
+        <meta name="theme-color" content="#17191a" />
         <meta name="description" content="My Personal WebSite" />
         <meta name="keywords" content="jon4s, jonas, jonas website, Jon4s" />
         <link rel="icon" href="/icon.png" />
       </Head>
       <div className=" h-screen w-screen inline-flex flex-col text-center pt-[50px]">
-        <h1 className="font-black text-6xl">Jonas</h1>
-        <h2 className="font-light pt-[10px] text-lg">Portugal, Aveiro, SMF</h2>
-        <p className="text-[1.25em] mt-[0.75em] font-light">
+        <h1 className="font-bold text-8xl">Jon4s</h1>
+        <h2 className="font-light pt-[10px] text-xl">Portugal, Aveiro, SMF</h2>
+        <p className="text-xl font-light">
           Web Developer • Student
         </p>
-        <div className="w-full justify-center text-5xl flex">
+        <div className="w-full justify-center text-6xl flex">
           <Github />
           <Discord />
           <Spotify />
