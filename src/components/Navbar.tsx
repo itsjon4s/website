c966fc7c5cd63f7c56088c6c186f159a2a0e4003
itsjon4s <motajoao670@gmail.com>
2022-08-22 00:14:05 +0100

    feat: add a transition in the navbar on mobile

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,7 @@
-import { Disclosure } from '@headlessui/react';
-import { MenuIcon, XIcon } from '@heroicons/react/solid';
+import { Disclosure, Transition } from '@headlessui/react';
+import { MenuIcon, XIcon } from '@heroicons/react/outline';
 import Image from 'next/image';
+import { Fragment } from 'react';
 
 export default function Navbar() {
   const navigation = [
@@ -35,22 +36,31 @@ export default function Navbar() {
                     <div className="ml-4 flex items-center md:ml-6"></div>
                   </div>
                   <div className="-mr-2 flex md:hidden">
-                    <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md  focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 ">
+                    <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md  focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white ">
                       {open ? <XIcon className="block h-6 w-6" aria-hidden="true" /> : <MenuIcon className="block h-6 w-6" aria-hidden="true" />}
                     </Disclosure.Button>
                   </div>
                 </div>
               </div>
-
-              <Disclosure.Panel className="md:hidden">
-                <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
-                  {navigation.map(item => (
-                    <Disclosure.Button key={item.name} as="a" href={item.href} className="block px-3 py-2 rounded-md text-base font-medium" aria-current="page">
-                      {item.name}
-                    </Disclosure.Button>
-                  ))}
-                </div>
-              </Disclosure.Panel>
+              <Transition
+                as={Fragment}
+                enter="transition ease-out duration-100"
+                enterFrom="transform opacity-0 scale-95"
+                enterTo="transform opacity-100 scale-100"
+                leave="transition ease-in duration-75"
+                leaveFrom="transform opacity-100 scale-100"
+                leaveTo="transform opacity-0 scale-95"
+              >
+                <Disclosure.Panel className="md:hidden">
+                  <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
+                    {navigation.map(item => (
+                      <Disclosure.Button key={item.name} as="a" href={item.href} className="block px-3 py-2 rounded-md text-xl font-medium" aria-current="page">
+                        {item.name}
+                      </Disclosure.Button>
+                    ))}
+                  </div>
+                </Disclosure.Panel>
+              </Transition>
             </>
           )}
         </Disclosure>
