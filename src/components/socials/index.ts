d04700859d19c01a31d516539dd041d224a4a1a4
Joao Mota <motajoao670@gmail.com>
2022-11-10 13:32:45 +0100

    ok

--- src/components/socials/index.ts
+++ src/components/socials/index.ts
@@ -1,7 +1,6 @@
-import Discord from './Discord';
 import Email from './Email';
 import Github from './Github';
 import Instagram from './Instagram';
 import Spotify from './Spotify';
 
-export { Discord, Email, Github, Instagram, Spotify };
+export { Email, Github, Instagram, Spotify };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,10 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
-import { Discord, Email, Github, Instagram, Spotify } from '../components/socials/index';
+import { Email, Github, Instagram, Spotify } from '../components/socials/index';
 
 const Home: NextPage = () => {
   return (
-    <div className="mx-10  ">
+    <div className="mx-10">
       <Head>
         <title>Jon4s</title>
         <meta name="theme-color" content="#8fcfd1" />
@@ -12,18 +12,16 @@ const Home: NextPage = () => {
       </Head>
       <div className="flex justify-center items-center h-screen pb-20">
         <div className="flex flex-col text-center">
-          <h1 className="font-black text-5xl ">
+          <h1 className="font-black text-6xl ">
             Hello, I am
             <span className="text-blue"> Jon4s</span>!
           </h1>
-          <h2 className="font-medium pt-[5px] text-xl text-subtext">Portugal</h2>
-          <p className="text-2xl font-normal pt-[15px] text-subtext">Web Developer • High School Student</p>
-          <div className="w-full justify-center text-6xl flex pt-[10px] space-x-3">
-            <Github />
-            <Discord />
-            <Spotify />
-            <Email />
+          <p className="text-3xl font-normal pt-[15px] text-subtext">Web Developer • High School Student</p>
+          <div className="w-full justify-center text-6xl md:text-7xl flex pt-[10px] space-x-3">
             <Instagram />
+            <Spotify />
+            <Github />
+            <Email />
           </div>
         </div>
       </div>
