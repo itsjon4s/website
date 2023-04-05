eb64a940a5d0d182e50a6e38da8a329e8d6e1360
Joao Mota <motajoao670@gmail.com>
2023-04-05 22:49:18 +0200

    chore: remove home component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
-import Home from './components/Home';
+import Socials from './components/Socials';
 
 const Page = () => {
-  return <Home />;
+  return (
+    <div className="flex items-center h-screen justify-center">
+      <div className="flex flex-col space-y-2 px-4">
+        <h1 className="font-bold text-4xl justify-center text-white animate-pulse">Jon4s</h1>
+        <p className="text2xl justify-center font-light">Web Developer • High School Student</p>
+        <Socials />
+      </div>
+    </div>
+    );
 }
 
 export default Page;
