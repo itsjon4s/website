b32cadf781d567cae2abfe8baa3f689b3fd8dcb4
Joao Mota <motajoao670@gmail.com>
2023-04-05 23:13:44 +0200

    fix: responsivity

--- app/components/Socials.tsx
+++ app/components/Socials.tsx
@@ -5,18 +5,18 @@ import { EnvelopeSimple, GithubLogo, InstagramLogo, SpotifyLogo } from 'phosphor
 
 const Socials = () => {
   return (
-    <div className="text-4xl  flex space-x-1">
+    <div className="text-4xl flex space-x-1">
       <Link href="https://www.instagram.com/_joao.mota_/">
-        <InstagramLogo className="scale-100 hover:scale-150 duration-300 cursor-pointer" />
+        <InstagramLogo  className="scale-100 text-white hover:scale-150 duration-300 cursor-pointer" />
       </Link>
       <Link href="https://open.spotify.com/user/0obnn3etn7vr9mafxwc0cfbvl">
-        <SpotifyLogo className="scale-100 hover:scale-150 duration-300 cursor-pointer" />
+        <SpotifyLogo className="scale-100 text-white hover:scale-150 duration-300 cursor-pointer" />
       </Link>
       <Link href="https://github.com/itsjon4s">
-        <GithubLogo className="scale-100 hover:scale-150 duration-300 cursor-pointer" />
+        <GithubLogo className="scale-100 text-white hover:scale-150 duration-300 cursor-pointer" />
       </Link>
       <Link href="mailto:motajoao670@gmail.com">
-        <EnvelopeSimple className="scale-100 hover:scale-150 duration-300 cursor-pointer" />
+        <EnvelopeSimple className="scale-100 text-white hover:scale-150 duration-300 cursor-pointer" />
       </Link>
     </div>
   );
